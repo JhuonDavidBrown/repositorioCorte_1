d111eccf2e6c99c346fdefc505802d8c4719754c
JounDavidBrown <vsvrrara@gmail.com>
2024-03-21 06:40:02 -0500

    version 2_3 esta funcional

--- Ejercicio2/solucion2_3/solucion2_3/main.cpp
+++ Ejercicio2/solucion2_3/solucion2_3/main.cpp
@@ -4,7 +4,7 @@
  * Created: 18/03/2024 9:42:53 a. m.
  * Author : vsvrr
  */ 
-#define  F_CPU 8000000UL
+#define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
@@ -14,68 +14,60 @@
 #define pul1 PC4
 #define pul2 PC5
 
-uint8_t contadorD1= 0;
+uint8_t contadorD1 = 0;
 uint8_t contadorD2 = 0;
+uint8_t modo = 0; // 0 = modo1, 1 = modo2
 
-uint8_t bPul = 0;
-uint8_t bModo = 0;
 void modo1(void);
 void modo2(void);
-void pulsadores();
 
-int main(void)
-{	
-	DDRD |= (1<<led1)|(1<<led2); //Definiendo puertos de los leds como salida
-	PORTD &= ~((1<<led1)|(1<<led2)); //  apagar los LEDs inicialmente
+int main(void) {
+	DDRD |= (1<<led1)|(1<<led2); // Definiendo puertos de los leds como salida
+	PORTD &= ~((1<<led1)|(1<<led2)); // Apagar los LEDs inicialmente
 	DDRC = 0b11001111;
-	
-	while (1)
-	{
-		pulsadores();
-		if(bModo=1){
-			modo1();
+
+	while (1) {
+		if ((PINC & (1<<pul1)) == op && (PINC & (1<<pul2)) != op) {
+			modo = 0; // Cambiar al modo1
+			} else if ((PINC & (1<<pul1)) != op && (PINC & (1<<pul2)) == op) {
+			modo = 1; // Cambiar al modo2
 		}
-		else{
+
+		if (modo == 0) {
+			modo1();
+			} else {
 			modo2();
 		}
 		_delay_ms(5);
-		
 	}
-		
 }
 
-void pulsadores(){
-	if( (pul1==op && pul2!=op)||(pul1!=op && pul2==op) ){
-		if(bPul==1){
-			bModo=~bModo;
-			bPul=0;
-		}
-		bPul=1;
-	}
-}
-void modo1(void)
-{
+void modo1(void) {
 	contadorD1++;
 	contadorD2++;
-	if(contadorD1==80){ //0.4s
-		PORTD =~(1<<led1);
-		contadorD1=0;
+
+	if (contadorD1 == 80) { // 0.4s
+		PORTD ^= (1<<led1); // Invertir el estado del LED1
+		contadorD1 = 0;
 	}
-	if(contadorD2==140){ //0.7s
-		PORTD =~(1<<led2);
-		contadorD2=0;
+
+	if (contadorD2 == 140) { // 0.7s
+		PORTD ^= (1<<led2); // Invertir el estado del LED2
+		contadorD2 = 0;
 	}
 }
-void modo2(void)
-{
+
+void modo2(void) {
 	contadorD1++;
 	contadorD2++;
-	if(contadorD1==180){ //0.9s
-		PORTD=~(1<<led1);
-		contadorD1=0;
+
+	if (contadorD1 == 180) { // 0.9s
+		PORTD ^= (1<<led1); // Invertir el estado del LED1
+		contadorD1 = 0;
 	}
-	if (contadorD2==40){ //0.2s
-		PORTD=~(1<<led2);
-		contadorD2=0;
+
+	if (contadorD2 == 40) { // 0.2s
+		PORTD ^= (1<<led2); // Invertir el estado del LED2
+		contadorD2 = 0;
 	}
 }
